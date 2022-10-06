6a37ea2ac25aae241d32b09e3c87db9814a09471
Mike McCarty <mike.p.mccarty@gmail.com>
2022-10-05 17:44:52 -0700

    Finished troubleshooting question 7 in my JS and added 10 hikes in content

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ console.log(swim);
 
 if (swim === 'yes' || swim === 'y') {
     // console.log('You did it with a yes!');
-    alert(visitorsName + ', well you have that up on Mike! He is not big on swimming but after being on a submarine you can go underwater for days on end and never get wet!');
+    alert(visitorsName + ', well you have that up on Mike! He is not big on swimming but after being on a submarine he went underwater for days on end and never got wet!');
 } else if (swim === 'no' || swim === 'n') {
     // console.log('You did it with a no!');
     alert(visitorsName + ", Mike cannot stand putting his head underwater so that is something you have in common.");
@@ -53,7 +53,7 @@ console.log(petDog);
 
 if (petDog === 'yes' || petDog === 'y') {
     // console.log('You did it with a yes!');
-    alert("You and Mike have this is common. Mike has 2 dogs, Laszlo and Blondie and both have lab in them.");
+    alert("You and Mike have this in common. Mike has 2 dogs, Laszlo and Blondie and both have lab in them.");
 } else if (petDog === 'no' || petDog === 'n') {
     // console.log('You did it with a no!');
     alert("You know it takes a lot of responsibility to take care of a dog so I can understand not everyone does. Mike has 2 dogs, Laszlo and Blondie.");
@@ -88,68 +88,82 @@ for (let i = 0; i < tries; i++) {
     // users guess
     let userGuess = prompt(`Mike has lived in two other countries besides the United States but visited a lot more. You have ${tries - i} attempts left. Between 5 & 20 what is the number of countries Mike has visited? ONLY enter a number.`);
     // Conditions comparison
-    // Users' guess not a number
-    if (userGuess !== typeof (mikesCountries)) {
-        prompt('Please enter a number and no combination of letters and numbers. PLEASE!!!');
-        console.log(`This is try #${i + 1}!`);
-        // Users guess too high & higher than 20
-    } else if (userGuess > mikesCountries && userGuess > 20) {
-        prompt('I\'m sorry your attempt was incorrect and WAY too high. Remember the number is between 5 & 20.');
+    // Users guess too high & higher than 20
+    if (userGuess > mikesCountries && userGuess > 20 && i < 3) {
+        alert('I\'m sorry your attempt was incorrect and WAY too high. Remember the number is between 5 & 20.');
         console.log(`This is try #${i + 1}! And your guess was WAY too high.`);
         // Users guess too high
-    } else if (userGuess > mikesCountries && userGuess < 20) {
-        prompt('I\'m sorry your attempt was incorrect and too high.');
+    } else if (userGuess > mikesCountries && userGuess < 20 && i < 3) {
+        alert('I\'m sorry your attempt was incorrect and too high.');
         console.log(`This is try #${i + 1}! And your guess was too high.`);
         // Users' guess too low & less than 5
-    } else if (userGuess < mikesCountries && userGuess < 5) {
-        prompt('I\'m sorry your attempt was incorrect and WAY too low. Remember the number is between 5 & 20.');
+    } else if (userGuess < mikesCountries && userGuess < 5 && i < 3) {
+        alert('I\'m sorry your attempt was incorrect and WAY too low. Remember the number is between 5 & 20.');
         console.log(`This is try #${i + 1}! And your guess was WAY too low.`);
         // Users' guess too low
-    } else if (userGuess < mikesCountries) {
-        prompt('I\'m sorry your attempt was incorrect and too low.');
+    } else if (userGuess < mikesCountries && i < 3) {
+        alert('I\'m sorry your attempt was incorrect and too low.');
         console.log(`This is try #${i + 1}! And your guess was too low.`);
         // Users' guess correct with score
     } else if (userGuess === mikesCountries) {
-        prompt(`CONGRATULATIONS! You guessed correctly on your ${i + 1} attempt. Your score is ${score1 - (2 * i)}.`);
+        alert(`CONGRATULATIONS! You guessed correctly on your ${i + 1} attempt. Your score is ${score1 - (2 * i)}.`);
         console.log(`This is try #${i + 1}! And your guess was correct.`);
-        return score1;
+        score1 = score1 - (2 * i);
+        break;
     } else {
-        prompt(`You have 0 attempts and the correct answer was 16.  On to the last question.`);
+        alert(`You have 0 attempts and the correct answer was 16.  On to the last question.`);
         score1 = 0;
+        console.log(mikesCountries);
+        console.log(userGuess);
         console.log(`This is try #${i + 1}! And you are out of guesses!`);
-        return score1;
     }
 }
 
+
 // Question 7
 // Array of countries
-let countries = ['KOREA', 'JAPAN', 'THAILAND', 'AUSTRALIA', 'PHILIPPINES',];
-// number of attempts
+let countries = ['KOREA', 'JAPAN', 'THAILAND', 'AUSTRALIA', 'PHILIPPINES'];
+// number of attempts allowed
 let attempts = 6;
+// number of attempts started
+let userAttempts = 0;
 // starting score
 let score2 = 10;
 
-for (let j = 0; j < attempts; i++) {
+while (userAttempts < attempts) {
     // users guess
-    let usersCountry = prompt(`Mike has visited 16 countries outside the United States but has five favorites. You have ${tries - i} attempts left. What do you believe is one of Mike's favorite countries? Do NOT enter any numbers & watch your spelling.`).toUpperCase();
+    let usersCountry = prompt(`Mike has visited 16 countries outside the United States but has five favorites. You have ${attempts - userAttempts} attempts left. What do you believe is one of Mike's favorite countries? Do NOT enter any numbers & watch your spelling.`).toUpperCase();
     // Conditions comparison
+    let guessCorrect = false;
+
     for (let k = 0; k < countries.length; k++) {
         // Users guess is correct
-        if (usersCountry === myArray[k]) {
-            console.log('You are correct with your country.');
-            prompt(`CONGRATULATIONS! You guessed correctly on your ${i + 1} attempt. Your score is ${score2 - (2 * i)}.`);
-            return score2;
+        if (usersCountry === countries[k]) {
+            score2 = score2 - (2 * userAttempts);
+            console.log(`You are correct with ${usersCountry}.`);
+            alert(`CONGRATULATIONS! You guessed correctly on your ${userAttempts + 1} attempt. Your score is ${score2 - (2 * userAttempts)}.`);
+            guessCorrect = true;
+            break;
             // Users guess is incorrect
-        } else if(usersCountry !== myArray[k]) {
-            console.log('You are incorrect with your country.');
-            prompt(`You guessed incorrectly on your ${i + 1} attempt.`);
-        } else {
-            prompt('You have 0 attempts and the correct answer was any of the following: Korea, Japan, Thailand, Australia, or the Philipines.');
-            score2 = 0;
-            console.log(`This is try #${i + 1}! And you are out of guesses!`);
-            return score2;
-        }
-        }
+        } 
+    }
+    if (guessCorrect) {
+        break;
     }
 
-    alert('Thank you ' + visitorsName + 'for visiting and have a great day!');
+    if (!guessCorrect && userAttempts < 5) {
+        alert(`You guessed incorrectly on your ${userAttempts + 1} attempt, and you have ${attempts - userAttempts - 1} attempts left.`);
+        console.log(`You are incorrect with your country and you have ${attempts - userAttempts - 1} left.`);
+    } 
+    if  (!guessCorrect && userAttempts === 5) {
+        alert('You have 0 attempts and the correct answer was any of the following: Korea, Japan, Thailand, Australia, or the Philipines.');
+        score2 = 0;
+        console.log(`This is try #${userAttempts + 1}! And you are out of guesses!`);
+        break;
+    }
+    userAttempts++;
+}
+
+alert(`Congratulations ${visitorsName} your total score for the last two questions was ${score1 + score2}!`);
+
+alert(`Thank you ${visitorsName} for visiting Mike\'s site and have a great day!`);
